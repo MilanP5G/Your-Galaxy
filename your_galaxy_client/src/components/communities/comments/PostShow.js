a67c5f0fd8c0da30cd27fb20100b2090af11dc83
MilanP5G <milan_parmar@hotmail.co.uk>
2020-10-09 21:05:31 +0100

    allowed posts comment to appear on posts show page

--- your_galaxy_client/src/components/communities/comments/PostShow.js
+++ your_galaxy_client/src/components/communities/comments/PostShow.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react'
 import CommentForm from './CommentForm'
+import Comment from './Comment'
 import '../../../styling/PostShow.css'
 
 
-const PostShow = ({ comment, match, posts }) => {
+const PostShow = ({ match, posts }) => {
 
   if (posts.posts.length === 0) return null;
   const post = posts.posts.find( post => post.id === parseInt(match.params.id))
@@ -13,10 +14,17 @@ const PostShow = ({ comment, match, posts }) => {
       <div className='post-show' >
        <h1>{post.title}</h1>
        <p>{post.content}</p>
+       Comments
+       <div className='comments'>
+         {post.comments.map((comment) => (
+           <Comment key={comment.id} comment={comment} postId={post.id} />
+         ))}
+       </div>
       </div>
       <div className='show-comment-form' >
        <CommentForm postId={post.id} />
       </div>
+
     </Fragment>
   )
 
